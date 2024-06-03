9562b85540726965d8df7eeeffe2edb0911c8505
krizmanmarko <mk4462@student.uni-lj.si>
2024-06-03 12:22:00 +0200

    successfully running primitive virtual machines

--- src/core/vm_run.c
+++ src/core/vm_run.c
@@ -1,5 +1,3 @@
-// TODO: use VM config struct
-
 #include "defs.h"
 #include "dtb.h"
 #include "memory.h"
@@ -29,12 +27,20 @@ init_hs_pgtable(struct vm_config *conf)
 	);
 
 	// devices
+	// TODO use struct config
 	map_page(conf->vm_pgtable, DTB_SERIAL, DTB_SERIAL, PTE_U | PTE_R | PTE_W);
 }
 
 static void
 init_hs(struct vm_config *conf)
 {
+	uint64 pgtable_pa;
+
+	init_hs_pgtable(conf);
+	pgtable_pa = KVA2PA((uint64) conf->vm_pgtable);
+	CSRW(hgatp, ATP_MODE_SV39 | pgtable_pa >> 12);
+	__asm__ volatile("hfence.gvma");
+
 	CSRW(hstatus, HSTATUS_VSXL | HSTATUS_SPV);
 	CSRW(hedeleg, 0ULL);
 	CSRW(hideleg, HIDELEG_VSTI);
@@ -46,10 +52,6 @@ init_hs(struct vm_config *conf)
 	CSRW(htimedelta, 0ULL);
 	CSRW(htval, 0ULL);
 	CSRW(htinst, 0ULL);
-
-	init_hs_pgtable(conf);
-	CSRW(hgatp, ATP_MODE_SV39 | ((KVA2PA((uint64) conf->vm_pgtable)) >> 12));
-	__asm__ volatile("hfence.gvma");
 }
 
 static void
@@ -68,18 +70,27 @@ init_vs()
 }
 
 static struct vm_config *
-get_config(uint64 hartid)
+get_config_for_cpu(uint64 hartid)
 {
-	for (int i = 0; i < config.nr_vms; i++)
-		if (config.vm[i].cpu_affinity & (1 << hartid))
-			return &config.vm[i];
-	return (struct vm_config *)0;
+	struct vm_config *conf = 0;
+	for (int i = 0; i < config.nr_vms; i++) {
+		if (config.vm[i].cpu_affinity & (1 << hartid)) {
+			if (conf == 0) {
+				conf = &config.vm[i];
+			} else {
+				panic("single CPU assigned to multiple VMs");
+			}
+		}
+	}
+	if (conf == 0)
+		panic("CPU active, but not assigned to any VM");
+	return conf;
 }
 
 void __attribute__((noreturn))
 vm_run(uint64 hartid)
 {
-	struct vm_config *conf = get_config(hartid);
+	struct vm_config *conf = get_config_for_cpu(hartid);
 
 	init_vs();
 	init_hs(conf);
