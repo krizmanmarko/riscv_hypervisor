097fae9b5cb4b94d6fff23cf6c4e84f92a40fb4c
krizmanmarko <mk4462@student.uni-lj.si>
2024-06-08 11:23:33 +0200

    vcpu now has non confusing indexing but wastes 8 bytes

---
+++ src/virtual/vcpu.c
@@ -0,0 +1,15 @@
+#include "defs.h"
+#include "riscv.h"
+#include "string.h"
+#include "types.h"
+#include "vcpu.h"
+#include "vm_config.h"
+
+void
+init_vcpu(struct vm_config *conf)
+{
+	memset(&conf->vcpu, '\x00', sizeof(struct vcpu));
+	CSRW(sscratch, &conf->vcpu);
+	// figure out which virtual hartid this is -> a0
+	(conf->vcpu).x[10] = 0x7;
+}
