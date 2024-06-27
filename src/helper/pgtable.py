33af7e24126be4b474bef49378ffe50f7f762f6c
krizmanmarko <mk4462@student.uni-lj.si>
2024-06-27 16:18:01 +0200

    clean pgtable printing

--- src/helper/pgtable.py
+++ src/helper/pgtable.py
@@ -25,161 +25,81 @@ def get_pgdirs(pa):
 		if pte == None:
 			continue
 		if pte & pte_rwx:
-			pgdirs[i] = pte2pa(pte)		# actual page
+			pgdirs[i] = pte		# actual page
 			continue
 		if pte & pte_v:
-			pa = pte2pa(pte)
-			pgdirs[i] = get_pgdirs(pa)
+			pgdirs[i] = get_pgdirs(pte2pa(pte))
 	return pgdirs
+
+def parse_pte(pte):
+	pa = pte2pa(pte)
+	r = 'r' if pte & (1 << 1) else '-'
+	w = 'w' if pte & (1 << 2) else '-'
+	x = 'x' if pte & (1 << 3) else '-'
+	return (pa, r, w, x)
+
+def get_va(i=None, j=None, k=None):
+	if i == None:
+		print('nope')
+		exit(1)
+	va = i << 9 << 9 << 12
+	if va & (1 << 38):
+		va |= 0xffffffc000000000
+
+	if j == None:
+		return va
+	va |= j << 9 << 12
+
+	if k == None:
+		return va
+	va |= k << 12
+	return va
+
+def print_pte(root, pte, i=None, j=None, k=None):
+	va = get_va(i, j, k)
+	pa, r, w, x = parse_pte(pte)
+	size = None
+	if i != None and j == None and k == None:
+		size = '1G'
+		j = -1
+		k = -1
+	elif i != None and j != None and k == None:
+		size = '2M'
+		k = -1
+	elif i != None and j != None and k != None:
+		size = '4K'
+	else:
+		print('something went wrong...')
+		exit(1)
+	print(f'{root:16x}[{i:3d}][{j:3d}][{k:3d}] {size} {r}{w}{x}: {hex(va)} -> {hex(pa)}')
 
 def print_pgtable_sv39(pa):
 	l2 = get_pgdirs(pa)
 	root = pa
 	for i, l2entry in enumerate(l2):
 		if isinstance(l2entry, int):
-			va = i << 9 << 9 << 12
-			if va & (1 << 38):
-				va |= 0xffffffc000000000
-			pa = l2entry
-			print('%16x[%3d] 1G: 0x%x -> 0x%x' % (root, i, va, pa))
+			print_pte(root, l2entry, i)
 		elif l2entry != None:
 			for j, l1entry in enumerate(l2[i]):
 				if isinstance(l1entry, int):
-					va = i << 9 << 9 << 12
-					va |= j << 9 << 12
-					if va & (1 << 38):
-						va |= 0xffffffc000000000
-					pa = l1entry
-					print('%16x[%3d][%3d] 2M: 0x%x -> 0x%x' % (root, i, j, va, pa))
+					print_pte(root, l1entry, i, j)
 				elif l1entry != None:
 					for k, l0entry in enumerate(l2[i][j]):
 						if l0entry != None:
-							va = i << 9 << 9 << 12
-							va |= j << 9 << 12
-							va |= k << 12
-							if va & (1 << 38):
-								va |= 0xffffffc000000000
-							pa = l0entry
-							print('%16x[%3d][%3d][%3d] 4K: 0x%x -> 0x%x' % (root, i, j, k, va, pa))
+							print_pte(root, l0entry, i, j, k)
 
-def print_pgtable_sv48(pa):
-	l3 = get_pgdirs(pa)
-	root = pa
-	for i, l3entry in enumerate(l3):
-		if isinstance(l3entry, int):
-			va = i << 9 << 9 << 9 << 12
-			if va & (1 << 47):
-				va |= 0xffff000000000000
-			pa = l3entry
-			print('%16x[%3d] 512G: 0x%x -> 0x%x' % (root, i, va, pa))
-		elif l3entry != None:
-			for j, l2entry in enumerate(l3[i]):
-				if isinstance(l2entry, int):
-					va = i << 9 << 9 << 9 << 12
-					va |= j << 9 << 9 << 12
-					if va & (1 << 47):
-						va |= 0xffff000000000000
-					pa = l2entry
-					print('%16x[%3d][%3d] 1G: 0x%x -> 0x%x' % (root, i, j, va, pa))
-				elif l2entry != None:
-					for k, l1entry in enumerate(l3[i][j]):
-						if isinstance(l1entry, int):
-							va = i << 9 << 9 << 9 << 12
-							va |= j << 9 << 9 << 12
-							va |= k << 9 << 12
-							if va & (1 << 47):
-								va |= 0xffff000000000000
-							pa = l1entry
-							print('%16x[%3d][%3d][%3d] 2M: 0x%x -> 0x%x' % (root, i, j, k, va, pa))
-						elif l1entry != None:
-							for l, l0entry in enumerate(l3[i][j][k]):
-								if l0entry != None:
-									va = i << 9 << 9 << 9 << 12
-									va |= j << 9 << 9 << 12
-									va |= k << 9 << 12
-									va |= l << 12
-									if va & (1 << 47):
-										va |= 0xffff000000000000
-									pa = l0entry
-									print('sv48 haha')
-									print(l0entry)
-									#print('%16x[%3d][%3d][%3d][%3d] 4K: 0x%x -> 0x%x' % (root, i, j, k, l, va, pa))
-
-def print_pgtable_sv57(pa):
-	l4 = get_pgdirs(pa)
-	root = pa
-	for i, l4entry in enumerate(l4):
-		if isinstance(l4entry, int):
-			va = i << 9 << 9 << 9 << 9 << 12
-			if va & (1 << 56):
-				va |= 0xff00000000000000
-			pa = l4entry
-			print('%16x[%3d] 256T: 0x%x -> 0x%x' % (root, i, va, pa))
-		elif l4entry != None:
-			for j, l3entry in enumerate(l4[i]):
-				if isinstance(l3entry, int):
-					va = i << 9 << 9 << 9 << 9 << 12
-					va |= j << 9 << 9 << 9 << 12
-					if va & (1 << 56):
-						va |= 0xff00000000000000
-					pa = l3entry
-					print('%16x[%3d][%3d] 512G: 0x%x -> 0x%x' % (root, i, j, va, pa))
-				elif l3entry != None:
-					for k, l2entry in enumerate(l4[i][j]):
-						if isinstance(l2entry, int):
-							va = i << 9 << 9 << 9 << 9 << 12
-							va |= j << 9 << 9 << 9 << 12
-							va |= k << 9 << 9 << 12
-							if va & (1 << 56):
-								va |= 0xff00000000000000
-							pa = l2entry
-							print('%16x[%3d][%3d][%3d] 1G: 0x%x -> 0x%x' % (root, i, j, k, va, pa))
-						elif l2entry != None:
-							for l, l1entry in enumerate(l4[i][j][k]):
-								if isinstance(l1entry, int):
-									va = i << 9 << 9 << 9 << 9 << 12
-									va |= j << 9 << 9 << 9 << 12
-									va |= k << 9 << 9 << 12
-									va |= l << 9 << 12
-									if va & (1 << 56):
-										va |= 0xff00000000000000
-									pa = l1entry
-									print('%16x[%3d][%3d][%3d][%3d] 2M: 0x%x -> 0x%x' % (root, i, j, k, l, va, pa))
-								elif l1entry != None:
-									for m, l0entry in enumerate(l4[i][j][k][l]):
-										if l0entry != None:
-											va = i << 9 << 9 << 9 << 9 << 12
-											va |= j << 9 << 9 << 9 << 12
-											va |= k << 9 << 9 << 12
-											va |= l << 9 << 12
-											va |= m << 12
-											if va & (1 << 56):
-												va |= 0xff00000000000000
-											pa = l0entry
-											print('%16x[%3d][%3d][%3d][%3d][%3d] 4K: 0x%x -> 0x%x' % (root, i, j, k, l, m, va, pa))
-											
-
-# if address is None then satp is read for pgtable
-# otherwise you supply the root page table address
-def pgtable(address=None, mode=0x8):
-	satp = int(gdb.execute('info registers satp', to_string=True).split()[1], 16)
+# supply the root page table address
+def pgtable(address):
 	# disable memory protection, so I can traverse page table
+	satp = int(gdb.execute('info registers satp', to_string=True).split()[1], 16)
+	hgatp = int(gdb.execute('info registers hgatp', to_string=True).split()[1], 16)
 	gdb.execute('set $satp = 0')
+	gdb.execute('set $hgatp = 0')
 
 	pa = address
-	if pa == None:
-		mode = satp >> 60
-		pa = satp & ~(0xf << 60)	# disable mode
-		pa = satp & ~(0xffff << 44)	# disable asid
-		pa <<= 12			# get phys address
-
 	try:
-		if mode == 0x8:
-			print_pgtable_sv39(pa)
-		elif mode == 0x9:
-			print_pgtable_sv48(pa)
-		elif mode == 0xa:
-			print_pgtable_sv57(pa)
+		print_pgtable_sv39(pa)
 	finally:
-		# on exit reenable satp
+		# on exit reenable satp and hgatp
 		gdb.execute('set $satp = ' + str(satp))
+		gdb.execute('set $hgatp = ' + str(hgatp))
