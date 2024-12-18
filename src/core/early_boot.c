0de62ff12481301277c0bc656e578326d420e4d6
krizmanmarko <mk4462@student.uni-lj.si>
2024-12-18 08:48:30 +0100

    init for C and jump to high address

--- src/core/early_boot.c
+++ src/core/early_boot.c
@@ -5,8 +5,6 @@ early_boot()
 {
 	// init uart: SBI handles it
 	log("\033[0mBooting!");
-	oops("%%p: %p", 0x63636363);
-	panic("%%s: %s", "hello world %s");
-	while (1);
 	//mem prot
+	while (1);
 }
